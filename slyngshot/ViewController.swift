2adab29ac3d915c6f419a84a0b7c031f86a5e40f
Chris Bodaglo <chrisbodaglo@chris-bodaglos-mac-pro.local>
2016-09-11 11:03:27 -0400

    Initial Commit

---
+++ slyngshot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slyngshot
+//
+//  Created by Chris Bodaglo on 2016-09-11.
+//  Copyright © 2016 Chris Bodaglo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
